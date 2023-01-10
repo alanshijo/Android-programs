900f1853447e32a43af411ad18c75e19ec81b8c0
Alan Shijo <94635672+alanshijo@users.noreply.github.com>
2023-01-10 12:46:20 +0530

    Toast msg on button click

---
+++ Toast/MainActivity.java
@@ -0,0 +1,24 @@
+package lottery.softsolutions.tost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button click=findViewById(R.id.button);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"clicked",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
